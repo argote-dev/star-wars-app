09fa5123d7b7f433b91b9caa518e501916504829
David Argote <44904958+argote-dev@users.noreply.github.com>
2022-03-12 11:51:26 -0500

    feat: font, onClick item

--- app/src/main/java/com/argote/api_star_wars/ui/screens/people/PeopleScreen.kt
+++ app/src/main/java/com/argote/api_star_wars/ui/screens/people/PeopleScreen.kt
@@ -1,6 +1,8 @@
 package com.argote.api_star_wars.ui.screens.people
 
-import androidx.compose.foundation.background
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
@@ -8,23 +10,35 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
 import com.argote.api_star_wars.R
 import com.argote.api_star_wars.domain.remote.Person
-import dagger.hilt.EntryPoint
-import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.flow.Flow
 
 @Composable
 fun PeopleScreen(navController: NavController, viewModel: PeopleViewModel) {
-    val data: Flow<PagingData<Person>> = viewModel.getPeople()
-    PeopleList(data)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+            ) {
+                Text(
+                    text = stringResource(id = R.string.people_title),
+                    style = MaterialTheme.typography.h1,
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+        }
+    ) {
+        val data: Flow<PagingData<Person>> = viewModel.getPeople()
+        PeopleList(data)
+    }
 }
 
 @Composable
@@ -43,9 +57,15 @@ fun PeopleList(people: Flow<PagingData<Person>>) {
 
 @Composable
 fun PeopleItemList(person: Person) {
+    val context: Context = LocalContext.current
     Card(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .clickable {
+                Toast
+                    .makeText(context, person.name, Toast.LENGTH_SHORT)
+                    .show()
+            },
         elevation = 8.dp,
         shape = CircleShape.copy(CornerSize(10))
     ) {
@@ -59,16 +79,6 @@ fun PeopleItemList(person: Person) {
                     modifier = Modifier.padding(end = 10.dp)
                 )
                 Text(text = person.name, style = MaterialTheme.typography.subtitle1)
-                Spacer(modifier = Modifier.width(10.dp))
-                Card(
-                    elevation = 0.dp,
-                    shape = CircleShape.copy(CornerSize(10)),
-                    modifier = Modifier
-                        .background(MaterialTheme.colors.primary)
-                        .padding(5.dp)
-                ) {
-                    Text(text = person.gender, Modifier.background(MaterialTheme.colors.primary))
-                }
             }
         }
     }
--- app/src/main/java/com/argote/api_star_wars/ui/theme/Type.kt
+++ app/src/main/java/com/argote/api_star_wars/ui/theme/Type.kt
@@ -2,15 +2,30 @@ package com.argote.api_star_wars.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.argote.api_star_wars.R
 
 // Set of Material typography styles to start with
+
+val StarWarsFont = FontFamily(
+    Font(R.font.star_wars_font)
+)
+
 val Typography = Typography(
+    h1 = TextStyle(
+        fontFamily = StarWarsFont,
+        fontSize = 21.sp
+    ),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = StarWarsFont,
         fontSize = 16.sp
     )
     /* Other default text styles to override
